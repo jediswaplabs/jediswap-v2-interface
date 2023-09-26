af2a068b05543609a512a3dafb2eb9a4009af780
Vinay Singh <vinay@mudrex.com>
2023-09-26 12:45:10 +0530

    routes setup

---
+++ src/pages/Swap/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Swap() {
+  return <div>swap</div>;
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,42 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Swap from "../pages/Swap";
+import Pool from "../pages/Pool";
+
+const Router = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/swap" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/pool" element={<Pool />} />
+      </Routes>
+      <Routes>
+        <Route path="/add" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/add/:currencyIdA" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/add/:currencyIdA/:currencyIdB" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/create" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/create/:currencyIdA" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/create/:currencyIdA/:currencyIdB" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/zap" element={<Swap />} />
+      </Routes>
+      <Routes>
+        <Route path="/remove/:currencyIdA/:currencyIdB" element={<Swap />} />
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default Router;
