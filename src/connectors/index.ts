10d98b8c0456b2f658027135e1a2938445226544
Vinay Singh <vinay@mudrex.com>
2023-12-11 22:24:21 +0530

    changed testnet and staging environment

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -11,7 +11,7 @@ export const isTestnetEnvironment = () => {
     return false
   }
   const host = new URL(String(location))?.host || ''
-  return host === 'app.testnet.jediswap.xyz'
+  return host === 'testnet.e2.jediswap.xyz'
 }
 
 export const isStagingEnvironment = () => {
@@ -22,7 +22,7 @@ export const isStagingEnvironment = () => {
     return false
   }
   const host = new URL(String(location))?.host || ''
-  return host === 'app.staging.jediswap.xyz'
+  return host === 'staging.e2.jediswap.xyz'
 }
 
 export const webWalletUrl = isTestnetEnvironment() ? 'https://web.hydrogen.argent47.net/' : 'https://web.argent.xyz/'
