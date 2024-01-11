750de2c4c7f565bf926590bb0e7a78846a3f0686
Vinay Singh <vinay@mudrex.com>
2024-01-11 23:51:30 +0530

    network handled for web3 status in header

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -20,6 +20,7 @@ import { shortenAddress } from 'utils'
 import { BaseButton, ButtonSecondary, ButtonSize, ThemeButton } from '../Button'
 import { RowBetween } from '../Row'
 import { useStarkName } from '@starknet-react/core'
+import { ChainId } from '@vnaysn/jediswap-sdk-core'
 
 const FULL_BORDER_RADIUS = 9999
 
@@ -33,13 +34,15 @@ const Web3StatusGeneric = styled(ButtonSecondary)`
   height: 38px;
   font-size: 16px;
   font-weight: 600;
-
-  :focus {
-    outline: none;
-  }
 `
 
-const Web3StatusConnectWrapper = styled.div``
+const Web3StatusConnectWrapper = styled.div`
+  font-family: 'Avenir LT Std';
+  background-color: ${({ theme }) => theme.surface5};
+  border: none;
+  color: ${({ theme }) => theme.white};
+  padding: 10px 24px;
+`
 
 const Web3StatusConnected = styled(Web3StatusGeneric)<{
   pending?: boolean
@@ -50,16 +53,32 @@ const Web3StatusConnected = styled(Web3StatusGeneric)<{
   border: none;
   color: ${({ theme }) => theme.white};
   padding: 10px 24px;
-  :hover,
-  :focus {
-    border: 2px solid ${({ theme }) => theme.white};
-  }
 `
 
-const Web3StatusConnecting = styled(Web3StatusConnected)`
-  &:disabled {
-    opacity: 1;
-  }
+const NetworkContainer = styled.div`
+  ${flexRowNoWrap};
+  width: 100%;
+  align-items: center;
+  border-radius: 8px;
+  cursor: pointer;
+  user-select: none;
+  height: 38px;
+  font-size: 16px;
+  font-weight: 600;
+  font-family: 'Avenir LT Std';
+  border: none;
+  color: ${({ theme }) => theme.white};
+  margin-right: 16px;
+  padding: 10px 24px;
+`
+
+const NetworkSelected = styled(Web3StatusGeneric)<{}>`
+  font-family: 'Avenir LT Std';
+  background-color: ${({ theme }) => theme.jediNavyBlue};
+  border: none;
+  color: ${({ theme }) => theme.white};
+  margin-right: 16px;
+  padding: 10px 24px;
 `
 
 const AddressAndChevronContainer = styled.div<{ loading?: boolean }>`
@@ -95,25 +114,26 @@ function Web3StatusInner() {
   const handleWalletDropdownClick = useCallback(() => {
     toggleAccountDrawer()
   }, [toggleAccountDrawer])
-  const { address, connector } = useAccountDetails()
+  const { address, connector, chainId } = useAccountDetails()
   const { data: starkName } = useStarkName({ address })
 
   if (address) {
     return (
-      <Web3StatusConnected data-testid="web3-status-connected" onClick={handleWalletDropdownClick}>
-        <StatusIcon account={address} connection={connector} size={40} />
-        <AddressAndChevronContainer>
-          <Text>{starkName ?? shortenAddress(address)}</Text>
-        </AddressAndChevronContainer>
-      </Web3StatusConnected>
+      <NetworkContainer>
+        <NetworkSelected data-testid="web3-status-connected" onClick={handleWalletDropdownClick}>
+          <Text>Starknet {chainId === ChainId.MAINNET ? 'Mainnet' : 'Goerli'}</Text>
+        </NetworkSelected>
+        <Web3StatusConnected data-testid="web3-status-connected" onClick={handleWalletDropdownClick}>
+          <StatusIcon account={address} connection={connector} size={40} />
+          <AddressAndChevronContainer>
+            <Text>{starkName ?? shortenAddress(address)}</Text>
+          </AddressAndChevronContainer>
+        </Web3StatusConnected>
+      </NetworkContainer>
     )
   } else {
     return (
-      <Web3StatusConnectWrapper
-        tabIndex={0}
-        // onKeyPress={(e) => e.key === 'Enter' && handleWalletDropdownClick()}
-        onClick={handleWalletDropdownClick}
-      >
+      <Web3StatusConnectWrapper tabIndex={0} onClick={handleWalletDropdownClick}>
         <StyledConnectButton tabIndex={-1} data-testid="navbar-connect-wallet">
           <Trans>Connect</Trans>
         </StyledConnectButton>
