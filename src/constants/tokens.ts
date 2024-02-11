c78825621a5b1c1a0ff0ef8a1ee62f8ef3a20521
princearoragithub <77785932+princearoragithub@users.noreply.github.com>
2024-02-11 23:53:21 +0530

    Update tokens.ts

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -33,7 +33,7 @@ export const FACTORY_ADDRESS = {
   [ChainId.GOERLI]: '0x06b4115fa43c48118d3f79fbc500c75917c8a28d0f867479acb81893ea1e036c',
 }
 export const NONFUNGIBLE_POOL_MANAGER_ADDRESS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x04d367b511dec42e5207c616e50afc8471ee8bdf53f46d1def9fc7d8411f1eec',
+  [ChainId.MAINNET]: '0x00287d2ff1c39a44cd18d9dc7ed5617c9cb16b65090db6a0f689aa14755e4e5e',
   [ChainId.GOERLI]: '0x039cda244be37e3361ee4bac45b264bb02b8b4425695fcf6f800629d2922714a',
 }
 export const SWAP_ROUTER_ADDRESS = {
