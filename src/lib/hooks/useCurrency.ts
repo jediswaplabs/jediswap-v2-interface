dbfdaa75a823f21bfd8488b12ef13b38e0a955ad
Vinay Singh <vinay@mudrex.com>
2024-02-15 22:54:34 +0530

    universal token search added

--- src/lib/hooks/useCurrency.ts
+++ src/lib/hooks/useCurrency.ts
@@ -5,8 +5,7 @@ import { ChainId, Currency, Token } from '@vnaysn/jediswap-sdk-core'
 import { useAccountDetails } from 'hooks/starknet-react'
 import { sendAnalyticsEvent } from 'analytics'
 import { isSupportedChain } from 'constants/chains'
-import { useBytes32TokenContract, useTokenContract } from 'hooks/useContract'
-import { NEVER_RELOAD, useSingleCallResult } from 'lib/hooks/multicall'
+import { useTokenContract } from 'hooks/useContractV2'
 import useNativeCurrency from 'lib/hooks/useNativeCurrency'
 import { useEffect, useMemo } from 'react'
 
@@ -14,6 +13,9 @@ import { DEFAULT_CHAIN_ID, DEFAULT_ERC20_DECIMALS, WETH } from '../../constants/
 // import { TOKEN_SHORTHANDS } from '../../constants/tokens'
 import { isAddress } from '../../utils'
 import { isAddressValidForStarknet } from 'utils/addresses'
+import { useContractRead } from '@starknet-react/core'
+import ERC20_ABI from 'abis/erc20.json'
+import { cairo, num, shortString } from 'starknet'
 
 // parse a name or symbol from a token response
 const BYTES32_REGEX = /^0x[a-fA-F0-9]{64}$/
@@ -30,6 +32,30 @@ function parseStringOrBytes32(str: string | undefined, bytes32: string | undefin
 export const UNKNOWN_TOKEN_SYMBOL = 'UNKNOWN'
 const UNKNOWN_TOKEN_NAME = 'Unknown Token'
 
+const useSingleCallResult = (address: string | undefined, type: string) => {
+  const { data: result, isLoading } = useContractRead({
+    functionName: type,
+    args: [],
+    abi: ERC20_ABI,
+    address,
+    watch: true,
+  })
+
+  return { result, isLoading }
+}
+
+export function parseStringFromArgs(data: any, isHexNumber?: boolean): string | undefined {
+  if (typeof data === 'string') {
+    if (isHexNumber) {
+      return num.hexToDecimalString(data)
+    } else if (shortString.isShortString(data)) {
+      return shortString.decodeShortString(data)
+    }
+    return data
+  }
+  return undefined
+}
+
 /**
  * Returns a Token from the tokenAddress.
  * Returns null if token is loading or null was passed.
@@ -38,40 +64,46 @@ const UNKNOWN_TOKEN_NAME = 'Unknown Token'
 export function useTokenFromActiveNetwork(tokenAddress: string | undefined): Token | null | undefined {
   const { chainId } = useAccountDetails()
 
-  const formattedAddress = isAddress(tokenAddress)
+  const formattedAddress = isAddressValidForStarknet(tokenAddress)
   const tokenContract = useTokenContract(formattedAddress ? formattedAddress : undefined, false)
-  const tokenContractBytes32 = useBytes32TokenContract(formattedAddress ? formattedAddress : undefined, false)
 
   // TODO (WEB-1709): reduce this to one RPC call instead of 5
   // TODO: Fix redux-multicall so that these values do not reload.
-  const tokenName = useSingleCallResult(tokenContract, 'name', undefined, NEVER_RELOAD)
-  const tokenNameBytes32 = useSingleCallResult(tokenContractBytes32, 'name', undefined, NEVER_RELOAD)
-  const symbol = useSingleCallResult(tokenContract, 'symbol', undefined, NEVER_RELOAD)
-  const symbolBytes32 = useSingleCallResult(tokenContractBytes32, 'symbol', undefined, NEVER_RELOAD)
-  const decimals = useSingleCallResult(tokenContract, 'decimals', undefined, NEVER_RELOAD)
+  const tokenName: any = useSingleCallResult(tokenContract?.address, 'name')
+  const symbol: any = useSingleCallResult(tokenContract?.address, 'symbol')
+  const decimals: any = useSingleCallResult(tokenContract?.address, 'decimals')
 
   const isLoading = useMemo(
-    () => decimals.loading || symbol.loading || tokenName.loading,
-    [decimals.loading, symbol.loading, tokenName.loading]
+    () => decimals.isLoading || symbol.isLoading || tokenName.isLoading,
+    [decimals.isLoading, symbol.isLoading, tokenName.isLoading]
   )
-  const parsedDecimals = useMemo(() => decimals?.result?.[0] ?? DEFAULT_ERC20_DECIMALS, [decimals.result])
 
-  const parsedSymbol = useMemo(
-    () => parseStringOrBytes32(symbol.result?.[0], symbolBytes32.result?.[0], UNKNOWN_TOKEN_SYMBOL),
-    [symbol.result, symbolBytes32.result]
-  )
-  const parsedName = useMemo(
-    () => parseStringOrBytes32(tokenName.result?.[0], tokenNameBytes32.result?.[0], UNKNOWN_TOKEN_NAME),
-    [tokenName.result, tokenNameBytes32.result]
+  const parsedDecimals = useMemo(
+    () => parseInt(decimals?.result?.decimals) ?? DEFAULT_ERC20_DECIMALS,
+    [decimals.result]
   )
 
   return useMemo(() => {
-    // If the token is on another chain, we cannot fetch it on-chain, and it is invalid.
-    if (typeof tokenAddress !== 'string' || !isSupportedChain(chainId) || !formattedAddress) return undefined
-    if (isLoading || !chainId) return null
+    if (!chainId || !formattedAddress || isLoading) return undefined
+    if (decimals.isLoading || symbol.isLoading || tokenName.isLoading) return null
+    const parsedTokenNameHexString =
+      tokenName && tokenName.result ? num.getHexString(tokenName?.result?.name.toString()) : UNKNOWN_TOKEN_NAME
 
-    return new Token(chainId, formattedAddress, parsedDecimals, parsedSymbol, parsedName)
-  }, [chainId, tokenAddress, formattedAddress, isLoading, parsedDecimals, parsedSymbol, parsedName])
+    const parsedSymbolHexString =
+      tokenName && tokenName.result ? num.getHexString(symbol?.result?.symbol.toString()) : UNKNOWN_TOKEN_SYMBOL
+
+    if (decimals.result) {
+      const token = new Token(
+        chainId,
+        formattedAddress,
+        parsedDecimals,
+        parseStringFromArgs(parsedSymbolHexString),
+        parseStringFromArgs(parsedTokenNameHexString)
+      )
+      return token
+    }
+    return undefined
+  }, [formattedAddress, chainId, decimals, symbol, tokenName])
 }
 
 type TokenMap = { [address: string]: Token }
