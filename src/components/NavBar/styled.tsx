a15e641ea43ca4db8c4e6bde5db9e683a6fa3b87
ishagoyal <iamishagoyal@gmail.com>
2024-02-23 17:41:32 +0530

    Optimise network name and address for mobile

--- src/components/NavBar/styled.tsx
+++ src/components/NavBar/styled.tsx
@@ -17,13 +17,17 @@ export const Nav = styled.nav`
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.lg}px) {
     padding: 1rem 64px;
   }
+
+  @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.sm}px`}) {
+    padding: 16px;
+  }
 `;
 
 export const LogoContainer = styled.div`
   display: flex;
   margin-right: 12px;
   align-items: center;
-  
+
   img {
     cursor: pointer;
   }
@@ -46,7 +50,7 @@ export const MenuContainer = styled.div`
   align-items: center;
   justify-content: space-around;
   margin: 0px 12px;
-  
+
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
     margin: 0px;
     display: flex;
@@ -70,7 +74,7 @@ const baseLinkCss = css`
   color: ${({ theme }) => theme.neutral1};
 
   :hover {
-    color: ${({ theme }) => darken(0.2, theme.neutral1)}
+    color: ${({ theme }) => darken(0.2, theme.neutral1)};
   }
 `;
 
@@ -91,16 +95,18 @@ export const StatusContainer = styled(Box)`
 export const MenuItem = styled(BaseMenuItem)``;
 
 export const ActiveMenuItem = styled(BaseMenuItem)`
-  text-shadow: 0px 0px 73.21151733398438px rgba(49, 255, 156, 0.50), 0px 0px 18.911256790161133px rgba(49, 255, 156, 0.70);
+  text-shadow: 0px 0px 73.21151733398438px rgba(49, 255, 156, 0.5), 0px 0px 18.911256790161133px rgba(49, 255, 156, 0.7);
   &:after {
-    content: "";
+    content: '';
     position: absolute;
     z-index: -1;
-   top: 0;
+    top: 0;
     width: 100%;
     height: 2px;
-    background: linear-gradient(0deg, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), linear-gradient(rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0) 100%);
-    box-shadow: rgba(49, 255, 156, 0.5) 0px 0px 18.9113px, rgba(49, 255, 156, 0.5) 0px 0px 73.2115px, rgba(49, 255, 156, 0.5) 0px 0px 7.32115px inset;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)),
+      linear-gradient(rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0) 100%);
+    box-shadow: rgba(49, 255, 156, 0.5) 0px 0px 18.9113px, rgba(49, 255, 156, 0.5) 0px 0px 73.2115px,
+      rgba(49, 255, 156, 0.5) 0px 0px 7.32115px inset;
   }
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
