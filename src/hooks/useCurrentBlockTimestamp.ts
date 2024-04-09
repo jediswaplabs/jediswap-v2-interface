62a2fb942778acc37394ec902ac2441dea63da3c
Vinay Singh <vinay@mudrex.com>
2024-04-09 13:51:26 +0530

    replacing last few pieces of feeder calls

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -4,9 +4,26 @@ import { useMemo } from 'react'
 import { useInterfaceMulticall } from './useContract'
 import { useMulticallContract } from './useContractV2'
 import { useSingleCallResult } from 'state/multicall/hooks'
+import { useAccountDetails } from './starknet-react'
+import { useContractRead } from '@starknet-react/core'
+import { MULTICALL_NETWORKS } from 'contracts/multicall'
+import { DEFAULT_CHAIN_ID } from 'constants/tokens'
+import { MULTICALL_ABI } from 'contracts/multicall'
 
-// gets the current timestamp from the blockchain
-export default function useCurrentBlockTimestamp(): BigNumber | undefined {
-  const multicall = useMulticallContract()
-  return useSingleCallResult(multicall, 'get_current_block_timestamp')?.result?.[0]
+const useCurrentBlockTimestamp = () => {
+  const { chainId } = useAccountDetails()
+  const { data: blockTimeStamp } = useContractRead({
+    functionName: 'get_current_block_timestamp',
+    args: [],
+    abi: MULTICALL_ABI,
+    address: MULTICALL_NETWORKS[chainId ?? DEFAULT_CHAIN_ID],
+    watch: true,
+  })
+
+  if (!blockTimeStamp) return undefined
+  const { block_timestamp } = blockTimeStamp as any
+
+  return BigNumber.from(block_timestamp.toString())
 }
+
+export default useCurrentBlockTimestamp
