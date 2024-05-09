9c5fc408ea1a80820efe137e54ed87315c448d74
Vinay Singh <vinay@mudrex.com>
2024-05-10 00:37:05 +0530

    rewards v2 phase 1 added

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -16,6 +16,34 @@ const TokenFields = `
   }
 `
 
+const PoolFields = `
+  fragment PoolFields on Pool {
+    poolAddress
+    token0 {
+      tokenAddress
+      symbol
+      name
+      #totalValueLocked
+    }
+    token1 {
+      tokenAddress
+      symbol
+      name
+      #totalValueLocked
+    }
+    volumeToken0
+    volumeToken1
+    volumeUSD
+    totalValueLockedUSD
+    totalValueLockedETH
+    totalValueLockedToken0
+    totalValueLockedToken1
+    token0Price
+    token1Price
+    fee
+  }
+`
+
 export const TOKENS_DATA = ({ tokenIds = [] }) => {
   const tokenString = `[${tokenIds.map((token) => `"${token}"`).join(',')}]`
 
@@ -24,6 +52,30 @@ export const TOKENS_DATA = ({ tokenIds = [] }) => {
     query tokensData {
       tokensData(first: 500, where: {tokenAddressIn: ${tokenString}}) {
         token{...TokenFields}
+        period
+      }
+    }
+  `
+  return gql(queryString)
+}
+
+export const HISTORICAL_POOLS_DATA = ({ tokenIds = [], periods = [] }) => {
+  const tokensString = `[${tokenIds.map((token) => `"${token}",`)}]`
+  const periodString = `[${periods.map((period) => `"${period}"`).join(',')}]`
+
+  let queryString = `
+    ${PoolFields}
+    query poolsData {
+      poolsData(
+        first: 500, 
+        where: {
+          periodIn: ${periodString},
+          bothTokenAddressIn: ${tokensString},
+        }
+      ) {
+        pool {
+          ...PoolFields
+        }
         period
       }
     }
