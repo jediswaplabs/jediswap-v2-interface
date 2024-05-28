343338f146675f4aa14ee783a4f6b2feb78bfe7e
Vinay Singh <vinay@mudrex.com>
2024-05-28 23:53:15 +0530

    added slippage warning to vault withdraw

--- src/components/vault/VaultWithdraw.tsx
+++ src/components/vault/VaultWithdraw.tsx
@@ -3,12 +3,20 @@ import { AutoColumn } from 'components/Column'
 import VaultWithdrawInput from './VaultWithdrawInput'
 import VaultWithdrawSummary from './VaultWithdrawSummary'
 import { useVaultActionHandlers, useVaultState, useVaultTokens } from 'state/vaults/hooks'
+import { ThemedText } from 'theme/components'
 
 const WithdrawWrapper = styled(AutoColumn)`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 12px;
+`
+
+const DisclaimerText = styled.div`
+  background: #4f2376;
+  padding: 8px 16px;
+  font-size: 16px;
+  border-radius: 10px;
 `
 
 function VaultWithdraw({ currentVault }: { currentVault: any }) {
@@ -25,20 +33,13 @@ function VaultWithdraw({ currentVault }: { currentVault: any }) {
       <VaultWithdrawInput
         value={withdrawTypedValue}
         onUserInput={onWithdrawInput}
-        // onMax={() => {
-        //   onFieldAInput(maxAmounts[Field.CURRENCY_A]?.toExact() ?? '')
-        // }}
-        // showMaxButton={!atMaxAmounts[Field.CURRENCY_A]}
         showMaxButton
-        // currency={currencies[Field.CURRENCY_A] ?? null}
         id="add-liquidity-input-tokena"
-        // fiatValue={currencyAFiat}
-        // showCommonBases
-        // locked={depositADisabled}
         vaultPair={pair}
         currentVault={currentVault}
       />
       <VaultWithdrawSummary id="add-liquidity-input-tokena" vaultPair={pair} currentVault={currentVault} />
+      <DisclaimerText>Withdrawal might incur small slippage (&lt;1%)</DisclaimerText>
     </WithdrawWrapper>
   )
 }
