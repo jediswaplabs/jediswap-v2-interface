35057673223cb85cd2292a99fff760db303fc68d
iamoskvin <ilya@acrylstore.ru>
2024-08-23 14:59:11 +0300

    claim august 30

--- src/pages/Rewards/index.tsx
+++ src/pages/Rewards/index.tsx
@@ -839,7 +839,7 @@ export default function Rewards() {
           </Coins> */}
         <RowBetween>
           <ClaimHeader>
-            <ClaimHeaderText>Next claim available on August 23</ClaimHeaderText>
+            <ClaimHeaderText>Next claim available on August 30</ClaimHeaderText>
             <img src={StarsIcon} style={{ marginLeft: '20px', marginBottom: '15px' }} />
           </ClaimHeader>
         </RowBetween>
